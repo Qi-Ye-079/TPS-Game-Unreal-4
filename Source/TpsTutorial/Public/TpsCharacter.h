8dedfa9d98d27d3eeea3b57af2ddb946481f19e8
Qi Ye <qye079@uottawa.ca>
2018-05-06 11:57:39 -0400

    Revert "T02 - Added axis inputs to Character."
    This reverts commit d61675705f949d544e37258fea2af12e8a19c1f0.

--- Source/TpsTutorial/Public/TpsCharacter.h
+++ Source/TpsTutorial/Public/TpsCharacter.h
@@ -19,10 +19,6 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	// Input functions for axis
-	void MoveForward(float axisValue);
-	void MoveRight(float axisValue);
-
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
