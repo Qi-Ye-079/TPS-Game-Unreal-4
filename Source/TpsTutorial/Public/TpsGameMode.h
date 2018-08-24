ed60ca1b6692ab1f536e5f613eebf73e41c10af7
Qi Ye <nanaya2024@gmail.com>
2018-08-24 15:09:07 -0400

    T50 - Created custom game mode base.

---
+++ Source/TpsTutorial/Public/TpsGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TpsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPSTUTORIAL_API ATpsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
